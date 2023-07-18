c6ec42f686a2a718f05699f7f8cec705c81b3ece
Marin <p1-mmr@users.noreply.github.com>
2023-07-18 09:53:31 +0200

    Correctly do the action to open the home page of the GUI in the web browser with a delay and a calculated file:/// URI when using the web UI launcher

--- src/gui/web_launcher.py
+++ src/gui/web_launcher.py
@@ -1,13 +1,22 @@
 #!/usr/bin/python3
 #-*- encoding: Utf-8 -*-
 from os.path import dirname, realpath
+from threading import Timer
+from pathlib import Path
 from sys import path
 
-path.insert(0, dirname(realpath(__file__)))
+GUI_DIR = dirname(realpath(__file__))
+STATIC_DIR = realpath(GUI_DIR + '/static')
+
+path.insert(0, GUI_DIR)
 
 from webbrowser import open_new_tab
 
-open_new_tab('file:///home/marin/hermes-dec/src/gui/static/index.html')
+home_path = Path(STATIC_DIR + '/index.html').as_uri()
+
+browser_launcher = Timer(1.0, open_new_tab, (home_path, ))
+browser_launcher.daemon = True
+browser_launcher.start()
 
 from server import main
 main()
