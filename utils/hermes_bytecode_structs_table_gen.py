cc48065c22fc8d789193d6d9df00a94938616143
Andrii Kurdiumov <kant2002@gmail.com>
2025-09-19 09:56:26 +0500

    Update HTML with Hermes opcodes

--- utils/hermes_bytecode_structs_table_gen.py
+++ utils/hermes_bytecode_structs_table_gen.py
@@ -10,7 +10,11 @@ UTILS_DIR = dirname(realpath(__file__))
 ROOT_DIR = realpath(UTILS_DIR + '/..')
 DOCS_DIR = realpath(ROOT_DIR + '/docs')
 
-GIT_TAGS = 'v0.0.1 v0.0.3 v0.1.0 v0.2.1 hbc70 hbc73 v0.7.0 v0.8.0 v0.8.1 v0.12.0'.split(' ')
+# Remove v0.8.1 since I cannot decipher what git_tags mean with bumping.
+# Also v0.8.1 (84) does not have any changes in the opcodes compared to v0.8.0.
+# But it clearly different from v0.8.0 (83) in published HTML.
+#GIT_TAGS = 'v0.0.1 v0.0.3 v0.1.0 v0.2.1 hbc70 hbc73 v0.7.0 v0.8.0 v0.8.1 v0.12.0'.split(' ')
+GIT_TAGS = 'v0.0.1 v0.0.3 v0.1.0 v0.2.1 hbc70 hbc73 v0.7.0 v0.8.0 v0.8.1 v0.12.0 hbc90 hbc92 hbc95'.split(' ')
 
 OUTPUT_FILE_NAME = DOCS_DIR + '/opcodes_table.html'
 
